454e786225c9a279a52820749dcffbe8a2fad93c
meetakshi253 <meetakshi19253@iiitd.ac.in>
2021-08-07 22:26:53 +0530

    Remove target=blank vulnerability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,12 @@ function Footer()
             <div className="Footer">
                 <div className="Footer-content-links Footer-item">
                       Connect with me:
-                      <a href="mailto:meetakshisty4@gmail.com" className="social" target="_blank"><FontAwesomeIcon icon={faEnvelope} size="lg" /></a>
-                      <a href="https://github.com/meetakshi253" className="social" target="_blank"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
-                      <a href="https://www.linkedin.com/in/meetakshisetiya/" className="social" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="lg" /></a>
-                      <a href="https://www.behance.net/meetakshi" className="social" target="_blank"><FontAwesomeIcon icon={faBehance} size="lg" /></a>
-                      <a href="https://www.instagram.com/metsetwho/" className="social" target="_blank"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
-                      <a href="https://www.last.fm/user/darthgraffiti" className="social" target="_blank"><FontAwesomeIcon icon={faLastfm} size="lg" /></a>
+                      <a href="mailto:meetakshisty4@gmail.com" className="social"><FontAwesomeIcon icon={faEnvelope} size="lg" /></a>
+                      <a href="https://github.com/meetakshi253" className="social"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
+                      <a href="https://www.linkedin.com/in/meetakshisetiya/" className="social"><FontAwesomeIcon icon={faLinkedin} size="lg" /></a>
+                      <a href="https://www.behance.net/meetakshi" className="social"><FontAwesomeIcon icon={faBehance} size="lg" /></a>
+                      <a href="https://www.instagram.com/metsetwho/" className="social"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
+                      <a href="https://www.last.fm/user/darthgraffiti" className="social"><FontAwesomeIcon icon={faLastfm} size="lg" /></a>
                 </div>
                 
                 <div className="Footer-content-copyright Footer-item">
