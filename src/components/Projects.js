b516a14b977e7df112f82d42338b1d9cdd51215c
meetakshi253 <meetakshi19253@iiitd.ac.in>
2021-08-08 02:44:47 +0530

    Add hover styles and Project Boilerplate

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { projectsData } from "../projectsData.js";
+
 function Projects()
-{return (
-    <div>
-        <p>Navbar</p>
-    </div>
+{
+    return (
+    <section id="Projects" className="content">
+        <div className="container">
+            <div className="container-projects">
+                <h1 className="section-title"><span>
+                    Projects
+                    </span>
+                </h1>
+       
+        <p className="container-message-details" id="projects-message">You can find all my projects on <a className="link" id="projects-message-link" href="https://github.com/meetakshi253/">Github.</a></p>
+        </div>
+        </div>
+    </section>
 )}
 
 export default Projects;
---
+++ src/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+    {
+    title: "React Reserve",
+    techstack: "ReactJs, CSS, Bootstrap",
+        description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "React Reserve",
+        techstack: "ReactJs, CSS, Bootstrap",
+            description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+            image: "./project-1.gif",
+            link: "https://reactbootcamp.com",
+        },
+        {
+            title: "React Reserve",
+            techstack: "ReactJs, CSS, Bootstrap",
+                description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+                image: "./project-1.gif",
+                link: "https://reactbootcamp.com",
+            },
+            {
+                title: "React Reserve",
+                techstack: "ReactJs, CSS, Bootstrap",
+                    description:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+                    image: "./project-1.gif",
+                    link: "https://reactbootcamp.com",
+                },
+                {
+                    title: "React Reserve",
+                    techstack: "ReactJs, CSS, Bootstrap",
+                        description:
+                      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+                        image: "./project-1.gif",
+                        link: "https://reactbootcamp.com",
+                    },
+]
