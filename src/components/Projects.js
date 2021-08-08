4332ecd03217801846eb8664e11bc98263dfb9ca
meetakshi253 <meetakshi19253@iiitd.ac.in>
2021-08-08 18:38:04 +0530

    Create Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,94 +1,124 @@
-import React from "react";
+import React, { Component } from "react";
+import { projectsData } from "../projectsData.js"
+import ProjectCard from "./ProjectCard"
 
-
-function Projects() {
-    return (
-        <section id="Projects" className="content">
-            <div className="container container-2">
-                <div className="container-projects">
-                    <h2 className="section-title"><span>
-                        Projects
-                    </span>
-                    </h2>
-                    <div className="container-message-details" id="projects-message">You can find all my projects on <a className="link" id="projects-message-link" href="https://github.com/meetakshi253/">Github.</a></div>
-                    <div className="all-cards">
-                    <div className="project-details">
-                        <div className="card">
-                            <img src="./project-1.png" alt="logo" className="card-image"/>
-                            <div className="card-body">
-                                <h1>Project</h1>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                            </div>
-
-                        </div>
-                        </div>
-                        <div className="project-details">
-                        <div className="card">
-                            <img src="./project-1.png" alt="logo" className="card-image"/>
-                            <div className="card-body">
-                                <h1>Project</h1>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                            </div>
-
-                        </div>
-                        </div>
-                        <div className="project-details">
-                        <div className="card">
-                            <img src="./project-1.png" alt="logo" className="card-image"/>
-                            <div className="card-body">
-                                <h1>Project</h1>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                            </div>
-
-                        </div>
-                        </div>
-                        <div className="project-details">
-                        <div className="card">
-                            <img src="./project-1.png" alt="logo" className="card-image"/>
-                            <div className="card-body">
-                                <h1>Project</h1>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                            </div>
-
-                        </div>
-                        </div>
-                        <div className="project-details">
-                        <div className="card">
-                            <img src="./project-1.png" alt="logo" className="card-image"/>
-                            <div className="card-body">
-                                <h1>Project</h1>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                            </div>
-
-                        </div>
-                        </div>
-                        <div className="project-details">
-                        <div className="card">
-                            <img src="./project-1.png" alt="logo" className="card-image"/>
-                            <div className="card-body">
-                                <h1>Project</h1>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                                <p className="project">Some details about the project</p>
-                            </div>
-
-                        </div>
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state = { projectsdata: projectsData }
+    }
+    render() {
+        const projectdetails = this.state.projectsdata.map((project) =>
+        (
+            <ProjectCard key={project.id} project={project} />
+        ));
+        return (
+            <section id="Projects" className="content">
+                <div className="container">
+                    <div className="container-projects">
+                        <h2 className="section-title"><span>Projects</span></h2>
+                        <div className="container-message-details" id="projects-message">You can find all my projects on <a className="link" id="projects-message-link" href="https://github.com/meetakshi253/">Github.</a></div>
+                        <div className="all-cards">
+                            {projectdetails}
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+
+        );
+    }
+
 }
 
 export default Projects;
+
+// function Projects() {
+//     return (
+//         <section id="Projects" className="content">
+//             <div className="container container-2">
+//                 <div className="container-projects">
+//                     <h2 className="section-title"><span>
+//                         Projects
+//                     </span>
+//                     </h2>
+//                     <div className="container-message-details" id="projects-message">You can find all my projects on <a className="link" id="projects-message-link" href="https://github.com/meetakshi253/">Github.</a></div>
+//                     <div className="all-cards">
+//                     <div className="project-details">
+//                         <div className="card">
+//                             <img src="./project-1.png" alt="logo" className="card-image"/>
+//                             <div className="card-body">
+//                                 <h3 className="card-body-title">Project</h3>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                             </div>
+
+//                         </div>
+//                         </div>
+//                         <div className="project-details">
+//                         <div className="card">
+//                             <img src="./project-1.png" alt="logo" className="card-image"/>
+//                             <div className="card-body">
+//                                 <h1>Project</h1>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                             </div>
+
+//                         </div>
+//                         </div>
+//                         <div className="project-details">
+//                         <div className="card">
+//                             <img src="./project-1.png" alt="logo" className="card-image"/>
+//                             <div className="card-body">
+//                                 <h1>Project</h1>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                             </div>
+
+//                         </div>
+//                         </div>
+//                         <div className="project-details">
+//                         <div className="card">
+//                             <img src="./project-1.png" alt="logo" className="card-image"/>
+//                             <div className="card-body">
+//                                 <h1>Project</h1>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                             </div>
+
+//                         </div>
+//                         </div>
+//                         <div className="project-details">
+//                         <div className="card">
+//                             <img src="./project-1.png" alt="logo" className="card-image"/>
+//                             <div className="card-body">
+//                                 <h1>Project</h1>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                             </div>
+
+//                         </div>
+//                         </div>
+//                         <div className="project-details">
+//                         <div className="card">
+//                             <img src="./project-1.png" alt="logo" className="card-image"/>
+//                             <div className="card-body">
+//                                 <h1>Project</h1>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                                 <p className="project">Some details about the project</p>
+//                             </div>
+
+//                         </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
+
---
+++ src/projectsData.js
@@ -0,0 +1,47 @@
+export const projectsData = [
+  { 
+    id: 1,
+    name: "Avis",
+    techstack: "R, Tableau, Twitter API",
+    details:
+      "An R program that extracts 2000 tweets each for Samsung Galaxy and Apple iPhone from four different cities and performs sentiment analysis on them. Data also visualised through Tableau.",
+    image: "./project-1.png",
+    link: "https://github.com/meetakshi253/Avis",
+  },
+  {
+    id: 2,
+    name: "Eyetastic",
+    techstack: "Python, GazeFlow API",
+    details:
+      "A Python program that enables scrolling, switching desktop, displacing and resizing active application window by eye movement and a webcam. Developed in a team of 4 for Vihaan'21 by IEEE DTU.",
+    image: "./project-2.png",
+    link: "https://github.com/meetakshi253/Eyetastic",
+  },
+  {
+    id: 3,
+    name: "Color Switch Reprise",
+    techstack: "Java, JavaFx",
+    details:
+      "A personal take on the classic mode of the popular game Color Switch. Used Object-Oriented program- ming practices and Event-Driven programming. Developed in a team of 2.",
+    image: "./project-3.png",
+    link: "https://github.com/APColab/ColorSwitch",
+  },
+  {
+    id: 4,
+    name: "shush",
+    techstack: "C, Makefile, Linux",
+    details:
+      "shush is a fully functional Linux program shell that was developed using several C and Linux libraries. The shell supports 10 commands and their various command-line options.",
+    image: "./project-4.png",
+    link: "https://github.com/meetakshi253/shush",
+  },
+  {
+    id: 5,
+    name: "The Half-Bubble Prince",
+    techstack: "Arduino, Processing, Sensors",
+    details:
+      "A Harry Potter themed multiplayer dodge-the-ball game that uses flex sensors on wands to shoot lasers and ultrasonic sensors on sliders to move the characters.",
+    image: "./project-5.png",
+    link: "https://reactbootcamp.com",
+  },
+]
