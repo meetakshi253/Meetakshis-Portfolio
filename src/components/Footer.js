89724ae6115c76395a3e21432a737113f40e8952
meetakshi253 <meetakshi19253@iiitd.ac.in>
2021-08-08 19:31:32 +0530

    Improve accessibility

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,12 @@ function Footer()
             <div className="Footer">
                 <div className="Footer-content-links Footer-item">
                       Connect with me:
-                      <a href="mailto:meetakshisty4@gmail.com" className="social"><FontAwesomeIcon icon={faEnvelope} size="lg" /></a>
-                      <a href="https://github.com/meetakshi253" className="social"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
-                      <a href="https://www.linkedin.com/in/meetakshisetiya/" className="social"><FontAwesomeIcon icon={faLinkedin} size="lg" /></a>
-                      <a href="https://www.behance.net/meetakshi" className="social"><FontAwesomeIcon icon={faBehance} size="lg" /></a>
-                      <a href="https://www.instagram.com/metsetwho/" className="social"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
-                      <a href="https://www.last.fm/user/darthgraffiti" className="social"><FontAwesomeIcon icon={faLastfm} size="lg" /></a>
+                      <a href="mailto:meetakshisty4@gmail.com" className="social"><FontAwesomeIcon icon={faEnvelope} size="lg"/><span className="visually-hidden">Email</span></a>
+                      <a href="https://github.com/meetakshi253" className="social"><FontAwesomeIcon icon={faGithub} size="lg" /><span className="visually-hidden">Github</span></a>
+                      <a href="https://www.linkedin.com/in/meetakshisetiya/" className="social"><FontAwesomeIcon icon={faLinkedin} size="lg" /><span className="visually-hidden">Linkedin</span></a>
+                      <a href="https://www.behance.net/meetakshi" className="social"><FontAwesomeIcon icon={faBehance} size="lg" /><span className="visually-hidden">Behance</span></a>
+                      <a href="https://www.instagram.com/metsetwho/" className="social"><FontAwesomeIcon icon={faInstagram} size="lg" /><span className="visually-hidden">Instagram</span></a>
+                      <a href="https://www.last.fm/user/darthgraffiti" className="social"><FontAwesomeIcon icon={faLastfm} size="lg" /><span className="visually-hidden">Last FM</span></a>
                 </div>
                 
                 <div className="Footer-content-copyright Footer-item">
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,20 @@
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
+
+
 function ProjectCard(prop) {
+    var newid = `${prop.project.name}`
+    console.log(newid)
     return (
         <div className="project-details">
             <div className="card">
                 <img src={prop.project.image} alt={prop.project.name} className="card-image" />
                 <div className="card-body">
                     <div className="card-body-title">
-                        <a className="nav-item" id="card-body-title-link" href={prop.project.link}>{prop.project.name}</a>
-                        <FontAwesomeIcon icon={faExternalLinkAlt} size="sm" />
+                        <a className="nav-item card-body-title-link" id={newid} href={prop.project.link}>{prop.project.name}</a>
+                        <FontAwesomeIcon icon={faArrowRight} size="sm" transform={{rotate: 315}}/>
                     </div>
                     <p className="card-body-details techstack">{prop.project.techstack}</p>
                     <p className="card-body-details">{prop.project.details}</p>
